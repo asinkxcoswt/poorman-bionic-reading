a3d404b19b45ade6dd446509eff70ca5feb990df
asinkxcoswt <asinkxcoswt@gmail.com>
2022-06-25 11:16:12 +0700

    fix bugs

--- bionify.js
+++ bionify.js
@@ -1,12 +1,12 @@
-var sel = window.getSelection(); // Gets selection
+const sel = window.getSelection(); // Gets selection
 if (sel.rangeCount) {
   // Creates a new element, and insert the selected text with the chosen font inside
-  var e = document.createElement("span");
+  const e = document.createElement("span");
   //   e.style = "color: red";
   e.innerHTML = bionifyText(sel.toString());
 
   // https://developer.mozilla.org/en-US/docs/Web/API/Selection/getRangeAt
-  var range = sel.getRangeAt(0);
+  const range = sel.getRangeAt(0);
   range.deleteContents(); // Deletes selected text…
   range.insertNode(e); // … and inserts the new element at its place
 }
